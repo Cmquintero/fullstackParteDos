92ca2c648118d4a385f11d4821959f5d1f153eaa
cmquintero <cmquinterot@ufpso.edu.co>
2025-04-08 02:31:41 -0500

    parte2 ejercicio 2.6

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,47 @@
-import Course from "./components/course";
+import { useState } from 'react'
+
 const App = () => {
-  const Midel ="Web Development Curriculum Mario";
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const handleNoteChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
     }
-  ]
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
   return (
     <div>
-      {courses.map(course => (
-        <Course course={course} midel={Midel} />
-      ))}
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNoteChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person,keyToname) => (
+          <li key={keyToname}>
+            <h3>{person.name}</h3>
+          </li>
+          
+        ))}
+      </ul>
     </div>
   )
 }
+
 export default App
