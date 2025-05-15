67b50085a940af10c0ef4402dba6b7140508a259
cmquintero <cmquinterot@ufpso.edu.co>
2025-05-15 00:53:10 -0500

    parte3 Ejercicio3.19

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react'
-import Note from './components/Note'
-import noteService from './services/notes'
+import personServices from './services/persons'
 import axios from 'axios'
 
 const Footer = () => {
@@ -32,9 +31,6 @@ const Notification = ({ message, succesMessage }) => {
   )
 }
 
-
-
-
 const Filter = ({ filterText, filterPerson }) => (
   <form>
     <div>
@@ -43,10 +39,10 @@ const Filter = ({ filterText, filterPerson }) => (
   </form>
 )
 
-const PersonForm = ({ newName, newNumber, handlepersonChange, handleNoteChange, handleSubmit }) => (
+const PersonForm = ({ newName, newNumber, handlepersonChange, handleNameChange, handleSubmit }) => (
   <form onSubmit={handleSubmit}>
     <div>
-      name: <input value={newName} onChange={handleNoteChange} />
+      name: <input value={newName} onChange={handleNameChange} />
     </div>
     <div>
       number: <input value={newNumber} onChange={handlepersonChange} />
@@ -72,75 +68,31 @@ const Persons = ({ persons, onDelete }) => (
   </ul>
 )
 
-
-
-
 const App = () => {
-  const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState('')
-  const [showAll, setShowAll] = useState(true)
-  const [errorMessage, setErrorMessage] = useState('You have a error papabello...')
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterText, setFilter] = useState('')
-  const [succesMessage, setSuccesMessage] = useState('Vas a agregar una persona?')
+  const [errorMessage, setErrorMessage] = useState('This view show you the error...')
+  const [succesMessage, setSuccesMessage] = useState('Do you want to add a person?')
   const [countries, setCountries] = useState([])
   const [searchItem, setSearchItem] = useState("")
-  const [selectedCountry, setSelectedCountry] = useState("")
-  const [weather, setWeather] = useState("")
-  const [apiError, setApiError] = useState("")
-
-
+  const [selectedCountry, setSelectedCountry] = useState(null)
+  const [weather, setWeather] = useState(null)
+  const [apiError, setApiError] = useState(null)
 
   useEffect(() => {
-    noteService.getAll().then(initialPersons => {
+    personServices.getAll().then(initialPersons => {
       setPersons(initialPersons)
     })
   }, [])
 
-  const toggleImportanceOf = id => {
-    const note = notes.find(n => n.id === id)
-    const changedNote = { ...note, important: !note.important }
-
-    noteService
-      .update(id, changedNote)
-      .then(returnedNote => {
-        setNotes(notes.map(n => n.id !== id ? n : returnedNote))
-      })
-      .catch(error => {
-        setErrorMessage(`Note '${note.content}' was already removed from server`)
-        setTimeout(() => setErrorMessage(null), 5000)
-        setNotes(notes.filter(n => n.id !== id))
-      })
-  }
-
-  const addNote = (event) => {
-    event.preventDefault()
-    const noteObject = {
-      content: newNote,
-      important: Math.random() > 0.5
-    }
-
-    noteService
-      .create(noteObject)
-      .then(returnedNote => {
-        setNotes(notes.concat(returnedNote))
-        setNewNote('')
-      })
-  }
-
-  const handleNoteChange = (event) => setNewNote(event.target.value)
   const handlepersonChange = (event) => setNewNumber(event.target.value)
   const handleNameChange = (event) => setNewName(event.target.value)
   const filterPerson = (event) => setFilter(event.target.value)
 
   const addPerson = (event) => {
     event.preventDefault()
-    setSuccesMessage("Has agregado una persona")
-    setTimeout(() => {
-      setSuccesMessage("")
-    }, 5000)
 
     const personExist = persons.find((person) => person.name === newName)
 
@@ -151,7 +103,7 @@ const App = () => {
 
       if (confirmUpdate) {
         const updatedPerson = { ...personExist, number: newNumber }
-        noteService
+        personServices
           .update(personExist.id, updatedPerson)
           .then(returnedPerson => {
             setPersons(persons.map(p =>
@@ -159,8 +111,8 @@ const App = () => {
             ))
             setNewName('')
             setNewNumber('')
-            setErrorMessage(`Updated ${returnedPerson.name}'s number`)
-            setTimeout(() => setErrorMessage(null), 5000)
+            setSuccesMessage(`Updated ${returnedPerson.name}'s number`)
+            setTimeout(() => setSuccesMessage(null), 5000)
           })
           .catch(error => {
             setErrorMessage(`Information of ${personExist.name} has already been removed from server`)
@@ -176,13 +128,19 @@ const App = () => {
       number: newNumber
     }
 
-    noteService.create(personObject).then(personBack => {
-      setPersons(persons.concat(personBack))
-      setNewName('')
-      setNewNumber('')
-      setErrorMessage(`Added ${personBack.name}`)
-      setTimeout(() => setErrorMessage(null), 5000)
-    })
+    personServices.create(personObject)
+      .then(personBack => {
+        setPersons(persons.concat(personBack))
+        setNewName('')
+        setNewNumber('')
+        setSuccesMessage(`Added ${personBack.name}`)
+        setTimeout(() => setSuccesMessage(null), 5000)
+      })
+      .catch(error => {
+        const backendError = error.response?.data?.error || 'Error adding person'
+        setErrorMessage(backendError)
+        setTimeout(() => setErrorMessage(null), 5000)
+      })
   }
 
   const deletePerson = (id) => {
@@ -190,12 +148,12 @@ const App = () => {
     const confirmDelete = window.confirm(`Do you want to delete this person in the form? ${person.name}?`)
 
     if (confirmDelete) {
-      noteService
+      personServices
         .remove(id)
         .then(() => {
           setPersons(persons.filter(persona => persona.id !== id))
           setSuccesMessage(`${person.name} has been deleted`)
-          setTimeout(() => setSuccesMessage(''), 5000)
+          setTimeout(() => setSuccesMessage(null), 5000)
         })
         .catch(error => {
           if (error.response && error.response.status === 404) {
@@ -210,10 +168,9 @@ const App = () => {
     }
   }
 
-  const notesToShow = showAll ? notes : notes.filter(note => note.important)
-  const personsToShow = filterText.length > 0 ? persons.filter(person => person.name && person.name.toLowerCase().includes(filterText.toLowerCase())) : persons;
-
-
+  const personsToShow = filterText.length > 0
+    ? persons.filter(person => person.name && person.name.toLowerCase().includes(filterText.toLowerCase()))
+    : persons
 
   useEffect(() => {
     if (searchItem.trim() === '') {
@@ -225,17 +182,13 @@ const App = () => {
       try {
         const url = `https://restcountries.com/v3.1/name/${searchItem}`
         const response = await axios.get(url)
-        const data = response.data
-        console.log("Países recibidos:", data)
-        setCountries(data)
+        setCountries(response.data)
         setWeather(null)
         if (response.data.length === 1) {
           const capital = response.data[0].capital && response.data[0].capital[0];
-
           errorWeather(capital)
         }
       } catch (error) {
-        console.error("Error fetching:", error.message)
         setCountries([])
       }
     }
@@ -245,106 +198,96 @@ const App = () => {
 
   const errorWeather = async (capital) => {
     try {
-
-      /*const v3 = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${apiKey}`*/
       const apiKey = import.meta.env.VITE_SOME_KEY
-      console.log("API Key:", apiKey); 
-      const v25 = `https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${apiKey}&units=metric`
-
-      const weatherResponse = await axios.get(v25)
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${apiKey}&units=metric`
+      const weatherResponse = await axios.get(url)
       setWeather(weatherResponse.data)
       setApiError(null);
-      console.log("API Key:", apiKey)
-
     } catch (error) {
       setWeather(null);
-      setApiError("fail to data")
+      setApiError("Failed to fetch weather data")
     }
   }
+
   const findLenguage = (languages) => {
     if (Array.isArray(languages)) {
       return languages.join(', ')
     } else if (typeof languages === 'object') {
       return Object.values(languages).join(', ')
     } else {
-      return 'unknow'
-
+      return 'unknown'
     }
-
   }
+
   const countryButton = (country) => {
     setSelectedCountry(country)
-    const capital = country.capital
-    errorWeather(capital)
+    const capital = country.capital && country.capital[0]
+    if (capital) errorWeather(capital)
   }
+
   return (
     <div>
-      <h1>Notes</h1>
-      <Notification message={errorMessage} />
-      <Notification succesMessage={succesMessage} />
+      <h1>Phonebook</h1>
+      <Notification message={errorMessage} succesMessage={succesMessage} />
 
-      <div>
-        <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all'}
-        </button>
-      </div>
-      <ul>
-        {notesToShow.map((note, i) =>
-          <Note
-            key={i}
-            note={note}
-            toggleImportance={() => toggleImportanceOf(note.id)}
-          />
-        )}
-      </ul>
-
-      <h2>Phonebook</h2>
       <Filter filterText={filterText} filterPerson={filterPerson} />
+
       <h2>Add New</h2>
       <PersonForm
         newName={newName}
         newNumber={newNumber}
-        handleNoteChange={handleNameChange}
+        handleNameChange={handleNameChange}
         handlepersonChange={handlepersonChange}
         handleSubmit={addPerson}
       />
+
       <h2>Numbers</h2>
       <Persons persons={personsToShow} onDelete={deletePerson} />
-      <h1 >Find country system</h1>
-      <p>find countries:
-        <input type="text" value={searchItem} onChange={(change) => setSearchItem(change.target.value)} placeholder='Enter the country to search' />
+
+      <h1>Find country system</h1>
+      <p>
+        find countries:
+        <input
+          type="text"
+          value={searchItem}
+          onChange={(e) => setSearchItem(e.target.value)}
+          placeholder='Enter the country to search'
+        />
       </p>
-      {countries.length > 10 && (
-        <p>You entered too many matches, Please be more specific.</p>
-      )}
+
+      {countries.length > 10 && <p>You entered too many matches, Please be more specific.</p>}
+
       {countries.length <= 10 && countries.length > 1 && (
         <div>
-          <h3>Matchin countries</h3>
+          <h3>Matching countries</h3>
           <ul>
             {countries.map((country) => (
-              <li key={country.name.common}>{country.name.common} <button onClick={() => countryButton(country)}>Show Data</button> </li>
+              <li key={country.name.common}>
+                {country.name.common}{' '}
+                <button onClick={() => countryButton(country)}>Show Data</button>
+              </li>
             ))}
           </ul>
         </div>
       )}
+
       {selectedCountry && (
         <div>
           <h3>{selectedCountry.name.common}</h3>
-          <p>Capital:{selectedCountry.capital}</p>
-          <p>Area:{selectedCountry.area}</p>
+          <p>Capital: {selectedCountry.capital && selectedCountry.capital[0]}</p>
+          <p>Area: {selectedCountry.area}</p>
           <p>Population: {selectedCountry.population}</p>
-          <p>Lenguage: {selectedCountry.languages && findLenguage(selectedCountry.languages)}</p>
+          <p>Language: {selectedCountry.languages && findLenguage(selectedCountry.languages)}</p>
           <img src={selectedCountry.flags.svg} alt={`Flag of ${selectedCountry.name.common}`} width="300" />
         </div>
       )}
-      <h3>Weather in Helsinki is good</h3>
-      {weather && (
+
+      {weather && selectedCountry && (
         <div>
-          <h3>Weather in {selectedCountry.capital[0]}</h3>
+          <h3>Weather in {selectedCountry.capital && selectedCountry.capital[0]}</h3>
           <p>Temperature: {weather.main.temp}°C</p>
           <p>Humidity: {weather.main.humidity}%</p>
           <p>Wind Speed: {weather.wind.speed} km/h</p>
-          <p>Weather Icon:</p>
           {weather.weather[0]?.icon && (
             <img
               src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
